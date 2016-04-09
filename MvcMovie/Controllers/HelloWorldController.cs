f2902db0b178ff9d42bea618ddaddc49c6135dfa
caofangsheng93 <fangsheng93@163.com>
2016-04-09 22:07:57 +0800

    Movie

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index(string path)
+        {
+            
+            ViewBag.cfs = path;
+          
+            return View();
+        }
+        public ActionResult Welcome(string name,int age)
+        {
+            ViewBag.Name =name;
+            ViewBag.Age = age;
+            return View();
+        }
+
+      
+    }
+}
